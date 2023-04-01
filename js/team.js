b45feb6e4d41f0496fe89d081e3f3488fa3a35e3
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2023-04-01 11:05:08 -0400

    made revision to my js and deleted some html

--- js/team.js
+++ js/team.js
@@ -1,6 +1,6 @@
 let member = {
     ceo : {
-        name: "The Foundress and CEO of the company.",
+        role: "The Foundress and CEO of the company.",
         headline: "Hello, I'm Kwency Dahilan ",
         bio: "As the founder and CEO of QUATRO, I recently took on the role of CEO, bringing with me five years of experience in the corporate world. As CEO, my focus is on driving growth and innovation, while maintaining the company's commitment to delivering exceptional service to our clients. I am known for my strategic thinking, my ability to inspire and motivate my team, and my passion for making a positive impact in the world.",
         skills: "#Photography",
@@ -10,7 +10,7 @@ let member = {
     },
 
     designer : {
-        name: "UI/UX Designer",
+        role: "UI/UX Designer",
         headline: "Hi, it is me again",
         bio: "Designing user interfaces is all about creating something that is not only visually appealing, but also functional and user-friendly. When I'm not working, I love to explore new places and take on new challenges. I truly believe that life is all about pushing oneself to be better and trying new things.  I work closely with developers and other designers to create seamless and engaging user experiences. I'm constantly pushing myself to come up with new and innovative designs, and I'm always up for a challenge. For me, the thrill of the unknown is what makes this job so exciting, and I wouldn't have it any other way.",
         skills: "#Snowboarding",
@@ -18,7 +18,7 @@ let member = {
     },
 
     programmer : {
-        name: "Programmer/Front-end Developer",
+        role: "Programmer/Front-end Developer",
         headline: "Yea right. Same woman!",
         bio: "I'm a problem solver at heart and I absolutely love to tackle complex coding puzzles. For me, there's nothing quite as satisfying as finally cracking a tough coding challenge. I'm responsible for developing software that meets our clients' needs. I'm constantly seeking out new and innovative solutions to complex problems, and I'm always pushing myself to be a better programmer. For me, coding is more than just a job – it's a passion, and I'm grateful to be able to do what I love every day",
         skills: "#Calligraphy",
